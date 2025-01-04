91b59c7b8f4ebb8527ae2f5b0cea4f5d71cdbd31
DileepaLakshan <145223312+DileepaLakshan@users.noreply.github.com>
2025-01-04 23:43:50 +0530

    add register user

---
+++ controllers/userController.js
@@ -0,0 +1,51 @@
+import asyncHandler from '../middleware/asyncHandler.js';
+import User from '../models/userModel.js'
+
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  
+
+  const userExist = await User.findOne( {email} );
+
+  if (userExist) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password
+  });
+
+  /*
+  if(user) {
+    genrateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  }else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+    */
+
+  res.status(201).json({
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    isAdmin: user.isAdmin,
+  });
+});
+
+
+export {
+    registerUser
+  };
--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import userRoutes from './routes/userRoutes.js'
 dotenv.config();
 
 connectDB(); // Connecct to MongoDB
 
 const app = express();
+
+//Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+//Cookie parser middleware
+app.use(cookieParser());
 
 //const port =process.env.PORT
 
@@ -13,6 +22,8 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 
 });
+
+app.use('/api/users', userRoutes);
 
 export default app;
 
