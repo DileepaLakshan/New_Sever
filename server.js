28270875ee4d7f25d168e02693c4ba5659b147b8
DileepaLakshan <145223312+DileepaLakshan@users.noreply.github.com>
2025-01-04 22:03:46 +0530

    add database

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+dotenv.config();
+
+connectDB(); // Connecct to MongoDB
 
 const app = express();
+
+const port =process.env.PORT
 
 app.get('/', (req, res) => {
   res.send('API is running...');
@@ -8,3 +15,5 @@ app.get('/', (req, res) => {
 });
 
 export default app;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
