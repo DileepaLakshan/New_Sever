2c20dba5e5d444de49624ed4b216fef59bbf34bc
Parakkrama Dasanayaka <110714055+Parakkrama24@users.noreply.github.com>
2025-01-05 13:21:02 +0530

    add cors

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ connectDB(); // Connecct to MongoDB
 
 const app = express();
 
+app.use(cors({
+  origin: 'https://new-sever.vercel.app', // Allow specific origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
+  credentials: true, // Allow cookies if needed
+}));
+
 //Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
