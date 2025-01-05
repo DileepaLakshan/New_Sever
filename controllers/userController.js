2ae09b32759ec2fd8713d0a2e723e557fbe3be00
Parakkrama Dasanayaka <110714055+Parakkrama24@users.noreply.github.com>
2025-01-05 14:16:58 +0530

    add login

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,39 @@ import asyncHandler from '../middleware/asyncHandler.js';
 import User from '../models/userModel.js';
 import genrateToken from '../utils/generatetoken.js';
 
+
+
+// @desc    Auth user & get token
+// @route   POST /api/users/login
+// @access  Public
+const authUser = asyncHandler(async (req, res) => {
+  
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if(user && (await user.matchPassword(password))) {
+
+    const token = genrateToken(res, user._id);
+
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: token
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
+
+
+});
+
+
+
+
 // @desc    Register a new user
 // @route   POST /api/users
 // @access  Public
