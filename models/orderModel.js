e0e71b78c5d3464e53ba634596f3677422f38f45
DileepaLakshan <145223312+DileepaLakshan@users.noreply.github.com>
2025-02-20 00:21:56 +0530

    Create orderModel.js

---
+++ models/orderModel.js
@@ -0,0 +1,68 @@
+import mongoose from 'mongoose';
+
+const orderSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User', // References the User model
+    },
+    orderItems: [
+      {
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: 'Product', // References the Product model
+        },
+        quantity: { type: Number, required: true },
+      },
+    ],
+    shippingAddress: {
+      addressLine1: { type: String,},
+      addressLine2: { type: String },
+      district: { type: String,},
+      province: { type: String,},
+      zipCode: { type: String,},
+    },
+    paymentMethod: {
+      type: String,
+    },
+    paymentResult: {
+      id: { type: String },
+      status: { type: String },
+      update_time: { type: String },
+      email_address: { type: String },
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    isPaid: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    paidAt: {
+      type: Date,
+    },
+    isDelivered: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    deliveredAt: {
+      type: Date,
+    },
+    deliveryState: {
+      type: String,
+      default: "Item Preparing",
+    },
+  },
+  {
+    timestamps: true, // Automatically adds createdAt and updatedAt fields
+  }
+);
+
+const Order = mongoose.model('Order', orderSchema);
+export default Order;
