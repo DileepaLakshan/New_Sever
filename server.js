a99a36075ea5dc78cbb9e60017475a258c2e8f08
DileepaLakshan <145223312+DileepaLakshan@users.noreply.github.com>
2025-02-19 23:45:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import connectDB from './config/db.js';
 import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 import userRoutes from './routes/userRoutes.js';
 import productRoutes from './routes/productRoutes.js';
-import cartRoutes from './routes/productRoutes.js';
+import cartRoutes from './routes/cartRoutes.js';
 import cors from 'cors';
 
 dotenv.config();
