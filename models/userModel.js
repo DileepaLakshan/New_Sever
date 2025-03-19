611fbbdb2430cc31addc3c217744252dee7330c2
DileepaLakshan <145223312+DileepaLakshan@users.noreply.github.com>
2025-03-19 23:21:05 +0530

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -33,19 +33,7 @@ const userSchema = mongoose.Schema(
           required: true,
           default: 1,
         },
-        price: {
-          type: Number,
-        },
-        name: {
-          type: String,
-          required: true,
-      
-        },
-        image: {
-          type: String,
-          required: true,
-      
-        },
+        
       },
     ],
   },
