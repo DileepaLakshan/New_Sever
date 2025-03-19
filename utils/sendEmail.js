c559bda09240b0f263a2e7e191709123902a13f3
DileepaLakshan <145223312+DileepaLakshan@users.noreply.github.com>
2025-03-20 01:37:53 +0530

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -6,7 +6,7 @@ dotenv.config();
 const sendEmail = async (to, subject, text) => {
     try {
 
-      console.log(process.env.EMAIL_USER);
+      console.log("test30");
       // Create transporter
       const transporter = nodemailer.createTransport({
         service: "gmail", // or use another SMTP service
@@ -15,7 +15,7 @@ const sendEmail = async (to, subject, text) => {
           pass: process.env.EMAIL_PASS, // Your email password or App Password
         },
       });
-  
+      console.log("test31");
       // Email options
       const mailOptions = {
         from: process.env.EMAIL_USER,
@@ -23,7 +23,7 @@ const sendEmail = async (to, subject, text) => {
         subject,
         text,
       };
-  
+      console.log("test32");
       // Send email
       const info = await transporter.sendMail(mailOptions);
       console.log("Email sent: " + info.response);
