3268e1c835ef6b5ef1d2751d8f8e59ce24bfe2ba
DileepaLakshan <145223312+DileepaLakshan@users.noreply.github.com>
2025-03-20 23:23:31 +0530

    Update orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import {
   createOrder,
   getOrderById,
+  updateOrderStatus,
   updateOrderToPaid,
   getMyOrders,
   getAllOrders,
@@ -13,7 +14,7 @@ const router = express.Router();
 
 router.route('/').post(protect, createOrder).get(protect, admin, getAllOrders);
 router.route('/myorders').get(protect, getMyOrders);
-router.route('/:id').get(protect, getOrderById);
+router.route('/:id').get(protect, getOrderById).put(protect, admin, updateOrderStatus);
 router.route('/:id/pay').put(protect, updateOrderToPaid);
 router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered);
 
